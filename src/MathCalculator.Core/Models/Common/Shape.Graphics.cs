fcf439d9c8ef099b7994d9600ad70524c92b7889
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-02-07 22:14:08 +0100

    added base class for Shapes

---
+++ src/MathCalculator.Core/Models/Common/Shape.Graphics.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace MathCalculator.Core.Models.Common
+{
+    public abstract partial class Shape
+    {
+        private PointF _location = new(250, 100);
+        private Color _color = Color.RoyalBlue;
+        private Control _control;
+
+        public PointF Location
+        {
+            get
+            {
+                return _location;
+            }
+            set
+            {
+                if (value == Location)
+                {
+                    return;
+                }
+
+                _location = value;
+                Invalidate();
+            }
+        }
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (value == Color)
+                {
+                    return;
+                }
+
+                _color = value;
+                Invalidate();
+            }
+        }
+        public Control Control
+        {
+            get
+            {
+                return _control;
+            }
+            set
+            {
+                if (value == Control)
+                {
+                    return;
+                }
+
+                _control = value;
+                Invalidate();
+            }
+        }
+
+        protected void Invalidate()
+        {
+            Control control = Control;
+
+            if (control is not null)
+            {
+                Draw(control);
+            }
+        }
+        protected abstract void Draw(Control control);
+    }
+}
