6a2b7789a75281dec095c6f07592ae3d91e0f34c
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-02-07 22:45:47 +0100

    updated logic for Square

--- src/MathCalculator.Core/Models/Design/Square.Graphics.cs
+++ src/MathCalculator.Core/Models/Design/Square.Graphics.cs
@@ -10,28 +10,12 @@ namespace MathCalculator.Core.Models
         /// 
         /// </summary>
         /// <param name="control"></param>
-        protected override void Draw(Control control)
-        {
-            if (control is not Form || control is not UserControl)
-            {
-                MessageBox.Show("Can't draw the shape", "Calculator", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            control.Paint += new PaintEventHandler(Draw_Shape);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="pe"></param>
-        private void Draw_Shape(object sender, PaintEventArgs pe)
+        public override void Draw(PaintEventArgs pe)
         {
             Graphics graphics = pe.Graphics;
             SolidBrush brush = new(Color);
-            PointF location = Location;
-            RectangleF rectangle = new(location.X, location.Y, _side, _side);
+            float side = Side * 10;
+            RectangleF rectangle = new(X, Y, side, side);
             graphics.FillRectangle(brush, rectangle);
             brush.Dispose();
         }
