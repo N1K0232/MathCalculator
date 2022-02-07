df41d3e5ab1b3a766ba0b734cc3f9cc9a44390cc
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-02-07 22:45:27 +0100

    tested controls

---
+++ src/MathCalculator/MathCalculatorForm.cs
@@ -0,0 +1,35 @@
+﻿using MathCalculator.Core.Models;
+using System;
+using System.Windows.Forms;
+
+namespace MathCalculator
+{
+    public partial class MathCalculatorForm : Form
+    {
+        public MathCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Square square = new();
+            square.Side = 12;
+            square.Name = "Square";
+            square.Draw(pe);
+
+            Rectangle rectangle = new();
+            rectangle.Width = 40;
+            rectangle.Height = 30;
+            rectangle.X = 400;
+            rectangle.Y = 300;
+            rectangle.Name = "Rectangle";
+            rectangle.Draw(pe);
+        }
+    }
+}
